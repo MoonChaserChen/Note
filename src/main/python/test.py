fe4c1c926894f1e937b5a82f9640aab431823fde
chenhouzhang <chin.kou.akira@gmail.com>
2023-12-05 10:46:49 +0800

    python 常用模块

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,30 +1,24 @@
-import datetime
+import json
 
-now = datetime.datetime.now()
-yesterday = now - datetime.timedelta(days=1)
-start = datetime.datetime(yesterday.year, yesterday.month, yesterday.day, 0, 0, 0)
-end = datetime.datetime(yesterday.year, yesterday.month, yesterday.day, 23, 59, 59)
-ts = start.timestamp()
-d = datetime.datetime.fromtimestamp(ts)
-# %a	Locale's abbreviated weekday name
-# %A	Locale's full weekday name
-# %b	Locale's abbreviated month name.
-# %B	Locale's full month name.
-# %c	Locale's appropriate date and time representation.
-# %d	Day of the month as a decimal number [01,31].
-# %H	Hour (24-hour clock) as a decimal number [00,23].
-# %I	Hour (12-hour clock) as a decimal number [01,12].
-# %m	Month as a decimal number [01,12].
-# %M	Minute as a decimal number [00,59].
-# %p	Locale's equivalent of either AM or PM.
-# %S	Second as a decimal number [00,61].
-# %U	Week number of the year (Sunday as the first day of the week) as a decimal number [00,53]. All days in a new year preceding the first Sunday are considered to be in week 0.
-# %w	Weekday as a decimal number [0(Sunday),6].
-# %W	Week number of the year (Monday as the first day of the week) as a decimal number [00,53]. All days in a new year preceding the first Monday are considered to be in week 0.
-# %x	Locale's appropriate date representation.
-# %X	Locale's appropriate time representation.
-# %y	Year without century as a decimal number [00,99].
-# %Y	Year with century as a decimal number.
-# %Z	Time zone name (no characters if no time zone exists).
-# %%	A literal "%" character.
-d_str = d.strftime('%Y-%m-%d %H:%M:%S')
+j = json.loads('{"id": 12, "name": "akira", "school": {"id": 123, "name": "华强北大学"}}')
+school_name = j["school"]["name"]
+s = json.dumps(j, ensure_ascii=False)
+
+
+class School:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+class Person:
+    def __init__(self, id, name, school):
+        self.id = id
+        self.name = name
+        self.school = school
+
+
+_school = School(123, "华强北大学")
+_person = Person(12, "akira", _school)
+s = json.dumps(_person, default=lambda o: o.__dict__, ensure_ascii=False, indent=4)
+p = Person(**json.loads(s))
