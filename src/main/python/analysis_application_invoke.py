0cc739a2b0aebb41922bb9ba502c9c71fb179d39
chenhouzhang <chin.kou.akira@gmail.com>
2023-12-05 10:11:58 +0800

    analysis_application_invoke.py

---
+++ src/main/python/analysis_application_invoke.py
@@ -0,0 +1,66 @@
+import requests
+import datetime
+import json
+import mysql.connector as mysql
+
+login_cookie = 'SameSite=none; isg=BO_vi1PK1ghPxdIWdI0B2ZjGfAX5lEO2Sl07DwF8qN5lUA5SCmT6BkRG0sDuMxsu; l=fBIIqffnPRQvgAZwBOfChurza779AIdbYuPzaNbMi9fP_r_J5CfcW1Eev-dvCn1VestD83-aK9byBLyJqP5R7-5zKdGItC0Z3dLHR3zC.; tfstk=eE6WjjNwRwBV2fholzZV5YCsyjJn2awaPDtdjMHrvLpJdBQVxBByUM8dRZb69ubFaJHdlEWyzgJJdMQ2eBfy8U8BdMb6VPyaQgjkKLUa7RPOoM4vp9p2TPIlqpm07PyaQgDClJPcx9cjsraCVWDoUv94jXzsllrNhnLHSa92mnWXJQLTIEMp0ttWNFI54U0w5yArOmOipnGucoGnt-2PdJX6Wn46wnx4aoZj_BOJmn93coGntQKD0NEbcfRC.; arms_user_parent_id=31320025; SameSite=none; XSRF-TOKEN=sxvtyUt4; aliyun_site=CN; login_aliyunid="chenhouzhang @ 31320025"; login_aliyunid_pk=31320025; login_aliyunid_sc=3R5H3e3HY2c8gwLZuY5GmS7K.1115vwDKiwGsUF7bV41gcmQucqqT4kqxsCbZ2cgngxqkp7z1.2mWNaj2owJrbmLKNMJmSC9ET9hjg3ronohWA5EkC7bRheA69Ght1XVHjoTu22VTDVs; login_aliyunid_ticket=3R5H3e3HY2c8gwLZuY5GmS7J.1118FLbBit88eciSLsSR4enxHHzzZmH7EtTG2Pt3eLUGzL19MTWLLibTvR147DgYnkkgkCyVuqADoioawoppDMm2QRDx78QxFBEkf2SDCZeguuqC1zdjsbK3wkz1bcSG2GJF2GjfMQ8R2LGU61LaP6qGtBmP4FbwXhoyxxH23MoYiH48yB3.2mWNaj25y9DYqQLgJPHpZFduhwTLLAfJmPKTmD1tvUAjTmbarMLx2PgzttGrnPEW7X; login_current_pk=205542499515429257; mfa_skip_cookie=sxDFil3X_Vs2KvTNa32xHyyVWN1GwpNvhO7Dfwlj0koR_Dij0qXf32TUWdbVyp953QjOq2pDZ61J102DEfNqB3Cgwn1Osb*1; login_aliyunid_csrf=_csrf_tk_1662001045752809; login_disaster=master; ORG_ID=6077b956afdac3288fbb56d1; LOGIN_ALIYUN_PK_FOR_TB=205542499515429257; TEAMBITION_SESSIONID=eyJ1aWQiOiI2NTRjOGMyNTY1NGVhYWRkYzZmNDFlOTkiLCJhdXRoVXBkYXRlZCI6MTcwMTY1NTU5ODc1OSwidXNlciI6eyJfaWQiOiI2NTRjOGMyNTY1NGVhYWRkYzZmNDFlOTkiLCJuYW1lIjoi6ZmI5Y6a55KLIiwiZW1haWwiOiJhY2NvdW50c182NTRjOGMyNTY1NGVhYWRkYzZmNDFlOTNAbWFpbC50ZWFtYml0aW9uLmNvbSIsImF2YXRhclVybCI6Imh0dHBzOi8vdGNzLWRldm9wcy5hbGl5dW5jcy5jb20vdGh1bWJuYWlsLzExMno1YzAwNzQ4MDA4MzdjNjgzYmYyM2YxNDRiYTE4NGIyNy93LzEwMC9oLzEwMCIsInJlZ2lvbiI6IiIsImxhbmciOiIiLCJpc1JvYm90IjpmYWxzZSwib3BlbklkIjoiIiwicGhvbmVGb3JMb2dpbiI6IiIsImNyZWF0ZWQiOiIyMDIzLTExLTA5VDA3OjM3OjA5LjU2OVoifSwibG9naW5Gcm9tIjoiIn0=; TEAMBITION_SESSIONID.sig=rYD24o0_P1CV3jmiReM2c6D2sNQ; ak_user_locale=zh_CN; currentRegionId=cn-beijing; aliyun_lang=zh; cna=IXPTHdEggWMCAbaWOaSes+l7; t=5246eb89c6547f85bb90e996ee6f7e12'
+url = "https://arms.console.aliyun.com/api/trace.json?action=TraceAction&eventSubmitDoGetDatas=1&source=nil"
+headers = {"Content-Type": "application/x-www-form-urlencoded;charset=utf-8", "Cookie": login_cookie}
+apps = {"voucher-service": "inaop@b245f0483fb7500", "voucher-task-service": "inaop@b0aad5e0aead514"}
+
+
+class QueryFilters:
+    def __init__(self, pid):
+        self.regionId = "cn-beijing"
+        self.pid = pid
+
+
+class QueryData:
+    def __init__(self, start_time, end_time, pid):
+        self.dimensions = ["rpc"]
+        self.metric = "appstat.incall"
+        self.measures = ["rt", "count", "error"]
+        self.intervalMillis = 2147483647
+        self.startTime = start_time
+        self.endTime = end_time
+        self.filters = QueryFilters(pid).__dict__
+
+    @staticmethod
+    def of_yesterday(_app):
+        now = datetime.datetime.now()
+        yesterday = now - datetime.timedelta(days=1)
+        start = datetime.datetime(yesterday.year, yesterday.month, yesterday.day, 0, 0, 0)
+        end = datetime.datetime(yesterday.year, yesterday.month, yesterday.day, 23, 59, 59)
+        return QueryData(int(start.timestamp() * 1000), int(end.timestamp() * 1000), apps[_app])
+
+    @staticmethod
+    def of_date(year, month, day, app):
+        start = datetime.datetime(year, month, day, 0, 0, 0)
+        end = datetime.datetime(year, month, day, 23, 59, 59)
+        return QueryData(int(start.timestamp() * 1000), int(end.timestamp() * 1000), apps[app])
+
+    def get_date(self):
+        return datetime.datetime.fromtimestamp(self.startTime/1000)
+
+    def __str__(self):
+        return "query=" + json.dumps(self.__dict__)
+
+
+for app in apps:
+    query_data = QueryData.of_yesterday(app)
+    r = requests.post(url, headers=headers, data=str(query_data))
+    j = json.loads(r.text)
+
+    connection = mysql.connect(host='localhost',
+                               port=3306,
+                               user='root',
+                               password='root@Mysql8.0',
+                               database='akira',
+                               charset='utf8mb4',
+                               auth_plugin='mysql_native_password')
+
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO `application_invoke_analysis` (`app_name`, `interface_name`, `request_date`, `response_time`, `request_cnt`) VALUES (%s, %s, %s, %s, %s)"
+            cursor.executemany(sql, list(map(lambda e: (app, e['rpc'], query_data.get_date(), int(e['rt']), int(e['count'])), j["data"]["data"])))
+        connection.commit()
--- src/main/python/test.py
+++ src/main/python/test.py
@@ -1,1 +1,30 @@
-print(list("abcd"))  # ['a', 'b', 'c', 'd']
+import datetime
+
+now = datetime.datetime.now()
+yesterday = now - datetime.timedelta(days=1)
+start = datetime.datetime(yesterday.year, yesterday.month, yesterday.day, 0, 0, 0)
+end = datetime.datetime(yesterday.year, yesterday.month, yesterday.day, 23, 59, 59)
+ts = start.timestamp()
+d = datetime.datetime.fromtimestamp(ts)
+# %a	Locale's abbreviated weekday name
+# %A	Locale's full weekday name
+# %b	Locale's abbreviated month name.
+# %B	Locale's full month name.
+# %c	Locale's appropriate date and time representation.
+# %d	Day of the month as a decimal number [01,31].
+# %H	Hour (24-hour clock) as a decimal number [00,23].
+# %I	Hour (12-hour clock) as a decimal number [01,12].
+# %m	Month as a decimal number [01,12].
+# %M	Minute as a decimal number [00,59].
+# %p	Locale's equivalent of either AM or PM.
+# %S	Second as a decimal number [00,61].
+# %U	Week number of the year (Sunday as the first day of the week) as a decimal number [00,53]. All days in a new year preceding the first Sunday are considered to be in week 0.
+# %w	Weekday as a decimal number [0(Sunday),6].
+# %W	Week number of the year (Monday as the first day of the week) as a decimal number [00,53]. All days in a new year preceding the first Monday are considered to be in week 0.
+# %x	Locale's appropriate date representation.
+# %X	Locale's appropriate time representation.
+# %y	Year without century as a decimal number [00,99].
+# %Y	Year with century as a decimal number.
+# %Z	Time zone name (no characters if no time zone exists).
+# %%	A literal "%" character.
+d_str = d.strftime('%Y-%m-%d %H:%M:%S')
